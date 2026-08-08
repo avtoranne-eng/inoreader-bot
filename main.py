7b5efd27d404229562fb05fc8c39dd3cde8fd379
avtoranne-eng <avtoranne@gmail.com>
2026-08-08 17:11:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def main():
         
         try:
             response = client.models.generate_content(
-                model='gemini-1.5-flash',
+                model='gemini-2.5-flash',
                 contents=prompt
             )
             create_google_doc(drive_service, docs_service, article.title, response.text)
