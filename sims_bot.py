5a82fc9341ad8377e2fc290123ba83d468b62532
avtoranne-eng <avtoranne@gmail.com>
2026-09-07 20:49:20 +0300

    Update sims_bot.py

--- sims_bot.py
+++ sims_bot.py
@@ -29,7 +29,7 @@ CATEGORY_URLS = [
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
 PROCESSED_FILE = "sims_processed.txt"
-MAX_MODS_PER_RUN = 10
+MAX_MODS_PER_RUN = 20
 MAX_PAGES = 50
 
 BLACKLIST = [
