949f2b97b7685029d5331e2a884a040f1ea1744d
avtoranne-eng <avtoranne@gmail.com>
2026-09-08 18:50:55 +0300

    Update sims_bot.py

--- sims_bot.py
+++ sims_bot.py
@@ -26,7 +26,7 @@ CATEGORY_URLS = [
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
 PROCESSED_FILE = "sims_processed.txt"
-MAX_MODS_PER_RUN = 20
+MAX_MODS_PER_RUN = 50
 MAX_PAGES = 50
 
 def clean_title(text):
